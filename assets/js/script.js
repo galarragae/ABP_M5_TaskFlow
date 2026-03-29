18cd56ebd94e822e750549dedbcca16d7fd94e0f
Elizabeth Galarraga G. <egalarragag@gmail.com>
2026-03-29 20:27:05 -0300

    Se hace merge con la rama leccion-4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
+const taskForm = document.getElementById("task-form");
+const tasksList = document.getElementById("tasks-list");
+
+// 1. Crear clase tarea (Task)
 class Task {
-  constructor(id, description, status, creationDate) {
+  constructor(id, taskDescription, status, creationDate, dueDate) {
     this.id = id; // número o string
-    this.description = description; // string (input)
+    this.taskDescription = taskDescription; // string (input)
     this.status = status; // boolean
     this.creationDate = creationDate; // string
+    this.dueDate = dueDate;
   }
 
   changeStatus() {
@@ -11,6 +16,7 @@ class Task {
   }
 }
 
+// 2. Crear clase gestor de tareas (TaskManager)
 class TaskManager {
   #tasks = [];
 
@@ -55,40 +61,157 @@ class TaskManager {
 
 // Instanciar las clases
 const taskManager = new TaskManager();
-taskManager.addTask(
-  new Task(1, "Tarea 1", true, new Date().toLocaleString("es-CL")),
-);
-taskManager.addTask(
-  new Task(2, "Tarea 2", false, new Date().toLocaleString("es-CL")),
-);
-taskManager.addTask(
-  new Task(3, "Tarea 3", true, new Date().toLocaleString("es-CL")),
-);
 
-// -------- Mostrar lista de tareas --------
-console.log(`\n-------- Mostrar lista de tareas --------\n`);
-console.log(taskManager.showTasksList());
+// Crear una alerta para que el usuario sepa que se está agregando la tarea
+const insertAlert = (className, message) => {
+  const alert = `
+  <div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+  `;
 
-// -------- Eliminar una tarea --------
-console.log(`\n-------- Eliminar una tarea --------\n`);
-taskManager.deleteTask(1);
-console.log(`Después de eliminar la tarea con id 1`);
-console.log(taskManager.showTasksList());
+  document.getElementById("alert-container").innerHTML = alert;
+};
 
+// ===============================================================
 
-// -------- Uso del objeto Date --------
-console.log(`\n-------- Uso del objeto Date --------\n`);
-const date = new Date();
-const [month, day, year, hours, minutes, seconds] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-  date.getHours(),
-  date.getMinutes(),
-  date.getSeconds(),
-];
+// Contador regresivo
 
-console.log(`${day}/${month + 1}/${year} - ${hours}:${minutes}:${seconds}`); // Enero es el mes 0, por eso: month + 1
-// Feha y hora en formato local:
-// Date -> objeto preconstruido de JS // 'es-CL' lenguaje: español, ubicación: Chile
-console.log(new Date().toLocaleString("es-CL"));
+const countdownTimer = (dueDate) => {
+  const actualDate = new Date().getTime();
+  const timingDifference = dueDate - actualDate; // Diferencia en milisegundos
+
+  const days = Math.floor(timingDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timingDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor(
+    (timingDifference % (1000 * 60 * 60)) / (1000 * 60),
+  );
+  const seconds = Math.floor((timingDifference % (1000 * 60)) / 1000);
+
+  return `${days} días, ${hours} horas, ${minutes} minutos y ${seconds} segundos`;
+};
+
+// Función para renderizar tareas
+const tasksListRendering = () => {
+  tasksList.innerHTML = "";
+  const tasksArray = taskManager.showTasksList();
+
+  tasksArray.forEach((task) => {
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-conten-center",
+      "align-items-center",
+    );
+
+    // SPAN para cuenta regresiva!!!
+    const span = document.createElement("span");
+    // dataset --> hace referencia a data-id, data-label, data-cualquierCosa
+    span.dataset.id = task.id;
+    span.classList.add("date-countdown", "small", "text-muted");
+    span.textContent = countdownTimer(task.dueDate);
+
+    const { taskDescription, status } = task;
+    li.textContent = `${taskDescription} ---> ${status ? "Completada" : "Pendiente"}`;
+
+    // Botones para eliminar y cambiar estado de una tarea
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-danger", "btn-sm", "delete-button");
+    deleteButton.textContent = "Eliminar";
+    deleteButton.dataset.id = task.id;
+
+    const changeButton = document.createElement("button");
+    changeButton.classList.add("btn", "btn-success", "btn-sm", "change-button");
+    changeButton.textContent = "Cambiar estado";
+    changeButton.dataset.id = task.id;
+
+    li.appendChild(span);
+    li.appendChild(changeButton);
+    li.appendChild(deleteButton);
+    tasksList.appendChild(li);
+  });
+};
+
+tasksListRendering(); // Para la primera carga de tareas
+
+// ===============================================================================
+// Actualizar el contador regresivo de cada tarea cada 1 segundo
+// ===============================================================================
+
+setInterval(() => {
+  // Capturar los <span> donde va cada cuenta regresiva
+  const countdown = document.querySelectorAll(".date-countdown");
+  countdown.forEach((span) => {
+    // Extraer el id del <span> (es el mismo de la tarea)
+    const id = Number(span.dataset.id);
+    // Encontrar la tarea correspondiente
+    const task = taskManager.showTasksList().find((task) => task.id === id);
+    if (task) {
+      // Si es que hay tarea, actualizar el temporizador de la respectiva tarea
+      span.textContent = countdownTimer(task.dueDate);
+    }
+  });
+}, 1000);
+
+// ===============================================================================
+// Evento submit para el form (agregar tareas a la lista renderizada)
+// ===============================================================================
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Insertar la alerta para que el usuario sepa que se está agregando la tarea
+
+  insertAlert(
+    "warning",
+    "<strong>Agregando tarea a la lista</strong>, por favor espere...",
+  );
+
+  // Simular un retraso al agregar tarea
+
+  setTimeout(() => {
+    const taskDescription = document.getElementById("task-description").value;
+    const dueDate = document.getElementById("due-date").value;
+    const deadline = dueDate ? new Date(dueDate).getTime() : undefined; // new Date().getTime() devuelve los milisegundos desde 01/01/1970
+    const newTask = new Task(
+      Date.now(), // id
+      taskDescription, // taskDescription
+      false, // status
+      new Date().toLocaleDateString("es-CL"), // creationDate
+      deadline, // dueDate
+    );
+
+    taskManager.addTask(newTask);
+
+    event.target.reset();
+    insertAlert("success", "¡Tarea agregada exitosamente a la lista!");
+
+    tasksListRendering();
+  }, 1000);
+});
+
+// ===============================================================================
+// Delegación de eventos: Event Listener (clicks en Eliminar y Cambiar estado)
+// ===============================================================================
+
+tasksList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-button")) {
+    const id = Number(event.target.dataset.id);
+    // console.log(event.target); // Para corroborar si está tomando el elemento que corresponde
+    taskManager.deleteTask(id);
+    tasksListRendering();
+  }
+});
+
+tasksList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("change-button")) {
+    const id = Number(event.target.dataset.id);
+
+    taskManager.changeStatus(id);
+    tasksListRendering();
+  }
+});
